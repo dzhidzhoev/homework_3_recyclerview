50868df0c963c977e8f9d08b925452c57f1f9dba
Vladislav Dzhidzhoev <dzhidzhoev@gmail.com>
2018-10-11 09:22:55 +0300

    Added RecyclerView.

--- app/src/main/java/com/tinkoff/androidcourse/MainActivity.java
+++ app/src/main/java/com/tinkoff/androidcourse/MainActivity.java
@@ -3,6 +3,7 @@ package com.tinkoff.androidcourse;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
+        RecyclerView recycler = findViewById(R.id.recycler_view);
         /**
          * Реализовать адаптер, выбрать любой LayoutManager и прикрутить это всё к RecyclerView
          *
